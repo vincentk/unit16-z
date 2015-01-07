03a6b27731dcd573b4adee19e7a76dd9e4b0e814
vincentk <vincent@192.168.2.109>
2015-01-07 13:48:36 +0100

    javadoc

--- src/main/java/com/unit16/z/Pool.java
+++ src/main/java/com/unit16/z/Pool.java
@@ -2,16 +2,26 @@ package com.unit16.z;
 
 import java.util.ArrayList;
 
+
+/**
+ * @param <D> type of cached element
+ */
 public abstract class Pool<D> {
 
 	private final ArrayList<D> cache_ = new ArrayList<>(1);
 
+	/**
+	 * @param val to be returned to the pool
+	 */
 	public final void recycle(D val)
 	{
 		assert !cache_.contains(val);
 		cache_.add(val);
 	}
 	
+	/**
+	 * Draw an element from the pool, creating one if the pool is empty.
+	 */
 	public final D draw()
 	{
 		if (cache_.isEmpty())
@@ -24,5 +34,8 @@ public abstract class Pool<D> {
 		}
 	}
 	
+	/**
+	 * Factory method for new objects.
+	 */
 	protected abstract D create();
 }
