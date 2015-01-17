5f23929897a95d8239081a734d373240fe00c49e
vincentk <vincent@192.168.2.114>
2015-01-17 16:47:19 +0100

    Closes #2, Closes #4

--- src/main/java/com/unit16/z/indexed/Empty.java
+++ src/main/java/com/unit16/z/indexed/Empty.java
@@ -2,7 +2,7 @@ package com.unit16.z.indexed;
 
 import java.util.Iterator;
 
-import com.google.common.collect.Iterators;
+import com.google.common.collect.ImmutableSet;
 
 final class Empty<M>
 extends DSL<M>
@@ -14,5 +14,5 @@ extends DSL<M>
 	public int size() { return 0; }
 
 	@Override
-	public Iterator<M> iterator() { return Iterators.emptyIterator(); }
+	public Iterator<M> iterator() { return ImmutableSet.<M>of().iterator(); }
 }
