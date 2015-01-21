8232a24fc2ac3111f42fbeb6f15c928034a9e64d
vincentk <vincent@192.168.2.114>
2015-01-21 18:46:45 +0100

    more java 8 cleanup

--- src/main/java/com/unit16/z/time/DateUtils.java
+++ src/main/java/com/unit16/z/time/DateUtils.java
@@ -3,6 +3,7 @@ package com.unit16.z.time;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.TimeZone;
 import java.util.function.Predicate;
 
@@ -10,43 +11,48 @@ import com.unit16.z.Sequence;
 
 public class DateUtils {
 
-	public static final Predicate<LocalDate> WEEKDAY = day -> day.getDayOfWeek().getValue() < 6;
-	
-	/**
-	 * @param start of sequence (inclusive)
-	 * @param end of sequence (inclusive)
-	 * @return all days between start and end -- empty sequence if start is after end.
-	 */
-	public static Sequence<LocalDate> period(final LocalDate start, final LocalDate end)
-	{
-		return new Sequence<LocalDate>(){
+    public static final Predicate<LocalDate> WEEKDAY = day -> day.getDayOfWeek().getValue() < 6;
 
-			private LocalDate today = start;
-			
-			@Override
-			public LocalDate next() { 
-				
-				final LocalDate r = today;
-				today = r.plusDays(1);
-				return r;
-			}
+    /**
+     * @param start of sequence (inclusive)
+     * @param end of sequence (inclusive)
+     * @return all days between start and end -- empty sequence if start is after end.
+     */
+    public static Sequence<LocalDate> period(final LocalDate start, final LocalDate end)
+    {
+        return new Sequence<LocalDate>(){
 
-			@Override
-			public boolean hasNext() {
-				return !today.isAfter(end);
-			}};
-	}
-	
-	public static LocalDateTime fromMicros(GMTMicros micros)
-	{
-	    final Instant ofEpochMilli = Instant.ofEpochMilli(micros.gmtMicros() / 1000);
-        return LocalDateTime.ofInstant(ofEpochMilli, UTC.toZoneId());
-	}
-	
-	public static GMTMicros.Const midnightUTCMicros(LocalDate date)
-	{
-	    return new GMTMicros.Const(date.atStartOfDay(UTC.toZoneId()).toEpochSecond() * 1000 * 1000);
-	}
-	
-	private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+            private LocalDate today = start;
+
+            @Override
+            public LocalDate next() { 
+
+                final LocalDate r = today;
+                today = r.plusDays(1);
+                return r;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return !today.isAfter(end);
+            }};
+    }
+
+    public static LocalDateTime fromMicros(GMTMicros micros)
+    {
+        final Instant ofEpochMilli = Instant.ofEpochMilli(micros.gmtMicros() / 1000);
+        return LocalDateTime.ofInstant(ofEpochMilli, UTC);
+    }
+
+    public static GMTMicros.Const UTCMicros(LocalDateTime date)
+    {
+        return new GMTMicros.Const(date.atZone(UTC).toEpochSecond() * 1000 * 1000);
+    }
+
+    public static GMTMicros.Const midnightUTCMicros(LocalDate date)
+    {
+        return new GMTMicros.Const(date.atStartOfDay(UTC).toEpochSecond() * 1000 * 1000);
+    }
+
+    private static final ZoneId UTC = TimeZone.getTimeZone("UTC").toZoneId();
 }
